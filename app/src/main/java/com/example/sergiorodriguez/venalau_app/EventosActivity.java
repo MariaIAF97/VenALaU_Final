7d450901aca11e8de210cb1856c7b06dd5a7263f
Maria Isabel Arcila Franco <maria.arcilaf@upb.edu.co>
2018-04-27 22:42:16 -0500

    Actividades
    Carga de espcios por API, Lista de eventos terminada

--- app/src/main/java/com/example/sergiorodriguez/venalau_app/EventosActivity.java
+++ app/src/main/java/com/example/sergiorodriguez/venalau_app/EventosActivity.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,91 +25,82 @@ import java.util.List;
 public class EventosActivity extends AppCompatActivity {
 
     ListView lvEventos;
-    FillList eventos;
+    FillListEventos eventos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
-        Log.d("creado", "creado");
+        Log.d("creado","creado");
         setContentView(R.layout.activity_eventos);
         lvEventos=(ListView)findViewById(R.id.lvEventos);
-        Log.d("creado", "creado");
-        eventos=new FillList();
+        Log.d("creado","creado");
+        eventos=new FillListEventos();
         eventos.execute();
-
     }
 
-    class FillList extends AsyncTask<String, String, Void> {
+    class FillListEventos extends AsyncTask<String,String,Void>{
+        private ProgressDialog progressDialog=new ProgressDialog(EventosActivity.this);
+        InputStream inputStream=null;
+        String result="";
 
-
-        private ProgressDialog progressDialog = new ProgressDialog(EventosActivity.this);
-        InputStream inputStream = null;
-        String result = "";
-
-        public FillList(){
+        public FillListEventos(){
 
         }
 
-        protected void onPreExecute() {
+        protected void onPreExecute(){
             progressDialog.setMessage("Downloading your data...");
             progressDialog.show();
             progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                public void onCancel(DialogInterface arg0) {
-                    FillList.this.cancel(true);
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    FillListEventos.this.cancel(true);
                 }
             });
+
         }
 
         @Override
-        protected Void doInBackground(String... params) {
-
-
-            try {
-                URL Url = new URL("http://http://venalau.azurewebsites.net/api/eventos/findall");
-                HttpURLConnection connection = (HttpURLConnection) Url.openConnection();
-                InputStream is = connection.getInputStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(is));
-                StringBuilder sb = new StringBuilder();
+        protected Void doInBackground(String... params){
+            try{
+                URL Url=new URL("http://venalau.azurewebsites.net/api/eventos/findAll");
+                HttpURLConnection connection=(HttpURLConnection) Url.openConnection();
+                InputStream is=connection.getInputStream();
+                BufferedReader br=new BufferedReader(new InputStreamReader(is));
+                StringBuilder sb=new StringBuilder();
                 String line;
-                while ((line = br.readLine()) != null) {
+                while((line=br.readLine())!=null){
                     sb.append(line);
                 }
                 connection.disconnect();
                 is.close();
-                result = sb.toString();
-            } catch (Exception e) {
-                Log.e("StringBuilding", "Error converting result " + e.toString());
+                result=sb.toString();
+
+
+
+            }catch(Exception e){
+                Log.e("StringBuilding","Error converting result "+e.toString());
             }
             return null;
         }
 
-        protected void onPostExecute(Void v) {
-            //parse JSON data
-            List<String> lista= new ArrayList<>();
-            try {
-                JSONArray jArray = new JSONArray(result);
-                for(int i=0; i < jArray.length(); i++) {
 
+        protected  void onPostExecute(Void v){
+            List<String> listaEventos=new ArrayList<>();
+            try{
+                JSONArray jArray=new JSONArray(result);
+                for(int i=0;i<jArray.length();i++){
                     JSONObject jObject = jArray.getJSONObject(i);
-
-                    String evento = jObject.getString("evento");
-                    lista.add(evento);
-                    //String tab1_text = jObject.getString("tab1_text");
-                    //int active = jObject.getInt("active");
-
-                } // End Loop
+                    String nombreEvento=jObject.getString("nombre");
+                    listaEventos.add(nombreEvento);
+                }
                 this.progressDialog.dismiss();
-            } catch (JSONException e) {
-                Log.e("JSONException", "Error: " + e.toString());
+            }catch(JSONException e){
+                Log.e("JSONException","Error: "+e.toString());
             }
 
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,
-                    lista);
-
+            ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,listaEventos);
             lvEventos.setAdapter(arrayAdapter);
+        }
 
-        } // protected void onPostExecute(Void v)
     }
 }
--- app/src/main/java/com/example/sergiorodriguez/venalau_app/ReservasActivity.java
+++ app/src/main/java/com/example/sergiorodriguez/venalau_app/ReservasActivity.java
@@ -1,13 +1,145 @@
 package com.example.sergiorodriguez.venalau_app;
 
+import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class ReservasActivity extends AppCompatActivity {
 
+    Spinner spEspacio;
+    FillList espacios;
+    private TextView txtFecha;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("creado","creado");
         setContentView(R.layout.activity_reservas);
+        spEspacio=(Spinner) findViewById(R.id.spEspacio);
+        Log.d("creado","creado");
+        espacios=new FillList();
+        espacios.execute();
+
+        txtFecha=(TextView)findViewById(R.id.txtFecha);
+        txtFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year=cal.get(Calendar.YEAR);
+                int month=cal.get(Calendar.MONTH);
+                int day=cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog=new DatePickerDialog(
+                        ReservasActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        mDateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month=month+1;
+
+                String date=month+"/"+day+"/"+year;
+                txtFecha.setText(date);
+            }
+        };
+
+
+
+    }
+
+    class FillList extends AsyncTask<String,String,Void> {
+        private ProgressDialog progressDialog=new ProgressDialog(ReservasActivity.this);
+        InputStream inputStream=null;
+        String result="";
+
+        public FillList(){
+
+        }
+
+        protected void onPreExecute(){
+            progressDialog.setMessage("Downloading your data...");
+            progressDialog.show();
+            progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    FillList.this.cancel(true);
+                }
+            });
+
+        }
+
+        @Override
+        protected Void doInBackground(String... params){
+            try{
+                URL Url=new URL("http://venalau.azurewebsites.net/api/espacio/findAll");
+                HttpURLConnection connection=(HttpURLConnection) Url.openConnection();
+                InputStream is=connection.getInputStream();
+                BufferedReader br=new BufferedReader(new InputStreamReader(is));
+                StringBuilder sb=new StringBuilder();
+                String line;
+                while((line=br.readLine())!=null){
+                    sb.append(line);
+                }
+                connection.disconnect();
+                is.close();
+                result=sb.toString();
+
+
+
+            }catch(Exception e){
+                Log.e("StringBuilding","Error converting result "+e.toString());
+            }
+            return null;
+        }
+
+
+        protected  void onPostExecute(Void v){
+            List<String> lista=new ArrayList<>();
+            try{
+                JSONArray jArray=new JSONArray(result);
+                for(int i=0;i<jArray.length();i++){
+                    JSONObject jObject = jArray.getJSONObject(i);
+                    String tipo=jObject.getString("tipo");
+                    lista.add(tipo);
+                }
+                this.progressDialog.dismiss();
+            }catch(JSONException e){
+                Log.e("JSONException","Error: "+e.toString());
+            }
+
+            ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_spinner_dropdown_item,lista);
+            spEspacio.setAdapter(arrayAdapter);
+        }
+
     }
 }
